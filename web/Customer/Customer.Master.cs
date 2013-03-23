4c172a49b392b7045598f6aeefd48cd1b65a8d2e
devsainii <devsainii@gmail.com>
2013-03-23 10:12:56 +0530

    To show opened ticket in Customer.Master

--- web/Customer/Customer.Master.cs
+++ web/Customer/Customer.Master.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
 
 namespace IDPRO.web.Customer
 {
@@ -11,6 +14,18 @@ namespace IDPRO.web.Customer
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            BindGrid();
+        }
+
+        public void BindGrid()
+        {
+            DataSet ds = new DataSet();
+            //ds.ReadXml(@"D:\Hosting\9847121\html\demo\course.xml");
+            //  ds.ReadXml(@"C:\Users\dev\Documents\Visual Studio 2010\WebSites\Driving2\course.xml");
+            string rootPath = System.Web.HttpContext.Current.Server.MapPath("~/");
+            ds.ReadXml(rootPath + "Ticket.xml");
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
 
         }
     }
